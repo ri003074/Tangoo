0b265e0b4db2a131515561a8a34a20a1c291e838
ri003074 <66888830+ri003074@users.noreply.github.com>
2020-11-20 06:53:25 +0900

    add s_c_counter, supplement field

--- backend/tangoo/models.py
+++ backend/tangoo/models.py
@@ -6,6 +6,9 @@ class Tangoo(models.Model):
     phrase_ja = models.CharField(max_length=200)
     phrase_en = models.CharField(max_length=200)
     word_en   = models.CharField(max_length=200)
+    supplement = models.TextField(default=None, blank=True)
+    s_counter = models.IntegerField(default=1)
+    c_counter = models.IntegerField(default=1)
 
     def __str__(self):
         return self.word_en
