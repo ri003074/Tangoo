caaf1431844bd15303292061de467c7bb919dfad
ri003074 <66888830+ri003074@users.noreply.github.com>
2020-11-24 22:01:21 +0900

    update display contents width

--- frontend/src/components/DisplayContents.vue
+++ frontend/src/components/DisplayContents.vue
@@ -3,10 +3,11 @@
     <table class="table">
       <tbody>
         <tr v-for="(content,index) in contents" v-bind:key=content.id>
-          <td style="width:17%;text-align:center;">{{ ('000' + index).slice(-3) }} ({{ ((content.c_counter / content.s_counter)*100).toFixed(1) }}%)</td>
-          <td style="width:29%;">{{ content.phrase_en }}</td>
-          <td style="width:29%;">{{ content.phrase_ja }}</td>
-          <td style="width:29%;">{{ content.word_en }}</td>
+          <td style="width:10%;text-align:center;">{{ ('000' + index).slice(-3) }}</td>
+          <td style="width:30%;">{{ content.phrase_en }}</td>
+          <td style="width:30%;">{{ content.phrase_ja }}</td>
+          <td style="width:20%;">{{ content.word_en }}</td>
+          <td style="width:10%;text-align:center;">{{ ((content.c_counter / content.s_counter)*100).toFixed(1) }}%</td>
         </tr>
       </tbody>
     </table>
