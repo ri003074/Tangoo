68a33e34aaf9a9acc50fe56b5ff2e86e8c5bcd04
ri003074 <ri003074@gmail.com>
2020-12-12 09:23:59 +0900

    clean up and fix favicon

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,17 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import DisplayContents from './components/DisplayContents.vue'
-// import HeaderDisplay from './components/HeaderDisplay.vue'
 import Quiz from './components/Quiz.vue'
 import AddContent from './components/AddContent.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { fas } from '@fortawesome/free-solid-svg-icons'
-// import { fab } from '@fortawesome/free-brands-svg-icons'
-// import { far } from '@fortawesome/free-regular-svg-icons'
 
-// library.add(fas, fab, far)
 library.add(fas, )
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
