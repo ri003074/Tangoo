0a90b7ba3a28d05f720733407be4810884b116ae
ri003074 <ri003074@gmail.com>
2020-12-12 09:59:51 +0900

    add delete function

--- frontend/src/components/DisplayContents.vue
+++ frontend/src/components/DisplayContents.vue
@@ -5,9 +5,10 @@
                 <tr v-for="(content,index) in contents" v-bind:key=content.id>
                     <td style="width:10%;text-align:center;">{{ ('000' + index).slice(-3) }}</td>
                     <td style="width:30%;">{{ content.phrase_en }}</td>
-                    <td style="width:30%;">{{ content.phrase_ja }}</td>
-                    <td style="width:20%;">{{ content.word_en }}</td>
+                    <td style="width:25%;">{{ content.phrase_ja }}</td>
+                    <td style="width:15%;">{{ content.word_en }}</td>
                     <td style="width:10%;text-align:center;">{{ ((content.c_counter / content.s_counter)*100).toFixed(1) }}%</td>
+                    <td style="width:10%;text-align:center;" class="delete_button" v-on:click="deleteContent(index)">[x]</td>
                 </tr>
             </tbody>
         </table>
@@ -15,14 +16,31 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     props:{
         contents:{
             type:Array,
         }
     },
+    methods:{
+        deleteContent(contentNumber){
+            let data = this.contents[contentNumber] // data for updte
+            this.contents.splice(contentNumber,1) //delete from contents
+
+            axios //delete from database
+                .delete("http://localhost:8000/api/" + data.id + "/", data)
+                .then(function(response){
+                    console.log(response.data)
+                })
+        }
+    }
 }
 </script>
 
 <style scoped lang="scss">
+.delete_button{
+    cursor: pointer;
+}
 </style>
