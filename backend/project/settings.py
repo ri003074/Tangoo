9cdb2388de78680c8af0d50aafaef713d70c3d3f
ri003074 <ri003074@gmail.com>
2020-12-19 18:41:23 +0900

    use sqlite3 for english study

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -80,12 +80,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'postgres',
-        'USER':'postgres',
-        'PASSWORD':'postgres',
-        'HOST':'db',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME':'postgres',
+        # 'USER':'postgres',
+        # 'PASSWORD':'postgres',
+        # 'HOST':'db',
+        # 'PORT':'5432',
     }
 }
 
